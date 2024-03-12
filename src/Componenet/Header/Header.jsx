865c59cd46326ea99744700dd4fb494ff035e9a2
munir79 <jakirhossainmunir79@gmail.com>
2024-03-12 11:13:06 +0600

    civer

--- src/Componenet/Header/Header.jsx
+++ src/Componenet/Header/Header.jsx
@@ -2,7 +2,7 @@ import profile from '../../assets/profile.png'
 
 const Header = () => {
     return (
-        <div className=' border-b-2 flex justify-between items-center p-4 mx-5'>
+        <div className=' border-b-2 flex justify-between items-center p-4 mx-5 max-w-7xl mx-auto '>
              <h1 className='  text-4xl  font-bold'>Knowledge Cafe</h1>
              <img src={profile} alt="" />
         </div>
--- src/SingleBlog/SingleBlog.jsx
+++ src/SingleBlog/SingleBlog.jsx
@@ -2,21 +2,27 @@
 import PropTypes from 'prop-types';
 const SingleBlog = ({cov}) => {
  console.log(cov);
-   const {title,cover,author_img,author,reading_time}=cov;
+   const {title,cover,author_img,author,reading_time,posted_date,hashtags}=cov;
     return (
-        <div>
-         <h3>{title} </h3> 
-         <img src={cover} alt="" />  
+        <div className='mb-20'>
+       
+         <img className='w-full' src={cover} alt="" />  
          <div className='flex justify-between'>
+
             <div className='flex'>
-              <img src={author_img} alt="" />
-              <p>{author}</p>
+              <img className='w-14' src={author_img} alt="" />
+              <div className='ml-6'>
+                <h3 className='text-2xl'>{author} </h3>
+                <p>{posted_date} </p>
+
+              </div>
             </div>
             <div>
-              <p>{reading_time} </p>
+              <span> {reading_time} min read </span>
             </div>
          </div>
-
+         <h2 className='text-4xl'>{title} </h2>
+         <a href="">{hashtags} </a>
         </div>
     );
 };
